4cc738137d84969373e94e27f8c5e00a6938e023
yomeans321 <luophilip@outlook.com>
2021-02-16 11:20:50 -0500

    Test

--- src/accidentproject/AccidentProject.java
+++ src/accidentproject/AccidentProject.java
@@ -44,6 +44,7 @@ public class AccidentProject extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        //This is trial
     }
     
 }
